5d97487d1c572302175c077c9d093589928ded27
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-10-07 16:28:28 +0200

    Añadido lanzaflechas (aun no va).

---
+++ VVVVV/Assets/Scripts/ArrowThrow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowThrow : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        StartCoroutine(ThrowArrow());
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator ThrowArrow()
+    {
+        while (true)
+        {
+            animator.Play("ArrowThrow");
+        }
+    }
+}
