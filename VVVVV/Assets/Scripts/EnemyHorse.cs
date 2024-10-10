caf585e1c2e6593ab56b0ad5be76b7b0d35ca50c
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-10-10 17:41:47 +0200

    Animación y movimiento de enemigo

--- VVVVV/Assets/Scripts/EnemyHorse.cs
+++ VVVVV/Assets/Scripts/EnemyHorse.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 public class EnemyHorse : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public LayerMask groundLayer;
     private GameObject enemyRaycast;
     private float direction = 1;
+    private float lastTime = -2;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
         enemyRaycast = transform.GetChild(0).gameObject;
+        gameObject.GetComponent<Animator>().SetBool("isMoving", true);
     }
 
     // Update is called once per frame
     void Update()
     {
         detectLimit();
+        Move();
     }
 
     public void Move()
     {
-        rb.velocity = new Vector2(4 * direction, rb.velocity.y);
-
+        rb.velocity = new Vector2(2 * direction, rb.velocity.y);
+        if (direction == 1)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }   
 
     }
     public void detectLimit()
     {
-        RaycastHit2D hit = Physics2D.Raycast(enemyRaycast.transform.position, Vector2.down * direction, 0.5f);
-        if (hit.collider == null)
+        RaycastHit2D hitDown = Physics2D.Raycast(enemyRaycast.transform.position, Vector2.down, 1f, groundLayer);
+        RaycastHit2D hitRight = Physics2D.Raycast(enemyRaycast.transform.position, Vector2.right * direction, 1f, groundLayer);
+
+        Debug.DrawRay(enemyRaycast.transform.position, Vector2.down, Color.red);
+        Debug.DrawRay(enemyRaycast.transform.position, Vector2.right * direction, Color.red);
+
+
+        if ((hitDown.collider == null || hitRight.collider != null) && lastTime+2 < Time.time)
         {
             direction *= -1;
+            lastTime = Time.time;
         }
+        
     }
+
 }
