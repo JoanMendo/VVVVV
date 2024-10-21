919e6b20a560bf0b8cd8afe4526418446cedcfb7
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-10-21 16:55:55 +0200

    Empezado canvas

---
+++ VVVVV/Assets/Scripts/Canvas.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    public Canvas canvas;
+    bool ispaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (ispaused == false)
+            {
+
+                Pause();
+                ispaused = true;
+            }
+            else
+            {
+                Resume();
+                ispaused = false;
+            }
+    }
+    
+
+    public void Pause()
+    {
+
+        canvas.enabled = true;
+        Time.timeScale = 0;
+
+    }
+    public void Resume()
+    {
+        canvas.enabled = false;
+        Time.timeScale = 1;
+
+    }
+
+    public void Restart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GameManager.currentScene);
+        Time.timeScale = 1;
+    }
+    public void Respawn()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<CharacterMovement>().Die();
+    }
+}
